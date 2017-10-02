312a88f62087b760aeae3ce354494b9efc814424
Diego de Santana Fernandes <diego@localhost.localdomain>
2017-10-01 21:47:21 -0300

    iniciando o projeto evenfapro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
+import { InscricaoComponent } from './inscricao/inscricao.component';
+import { EventoComponent } from './evento/evento.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        NavComponent,
+        FooterComponent,
+        InscricaoComponent,
+        EventoComponent
+    ],
+    imports: [
+        BrowserModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
